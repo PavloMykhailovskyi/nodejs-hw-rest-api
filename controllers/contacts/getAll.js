995e3810d481c6a9272ace21c7186698f1518443
PavloMykhailovskyi <100272784+PavloMykhailovskyi@users.noreply.github.com>
2023-01-24 20:41:14 +0200

    fix

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,12 +2,12 @@ const Contact = require('../../models/contact');
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { favorite } = req.body;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
   const contacts = await Contact.find({ owner: _id }, '', {
     skip,
     limit: Number(limit),
+    favorite
   }).populate('owner', '_id email');
   const favoriteContacts = contacts.filter((contact) => contact.favorite);
   const result = favorite ? favoriteContacts : contacts;
